0d24b8518fb428a7cebfbf85cd99ddf5711f620e
caub <cyril.auburtin@gmail.com>
2017-11-18 19:27:03 +0100

    rollup

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 var clocks = Array.from('🕛🕐🕑🕒🕓🕔🕕🕖🕗🕘🕙🕚🕧🕜🕝🕞🕟🕠🕡🕢🕣🕤🕥🕦');
 
-function emojiTime(date) {
+module.exports = function emojiTime(date) {
   var d = date || new Date();
   var m = d.getMinutes();
   var h = (m > 45 ? d.getHours() + 1 : d.getHours()) % 12;
   var isHalf = m > 15 && m <= 45;
   return clocks[isHalf ? 12 + h : h];
 };
-
-exports['default'] = emojiTime;
-module.exports = emojiTime;
---
+++ rollup.config.js
@@ -0,0 +1,8 @@
+export default {
+  name: 'emojiTime',
+  input: 'index.js',
+  output: {
+    file: 'dist/index.js',
+    format: 'umd'
+  }
+}
